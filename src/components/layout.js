8dd0990d63381de4c5bd28861d4341894c980e83
matthew-ardi <mechttam@gmail.com>
2019-07-17 21:03:06 -0500

    updated footer and cleaned up content from starter template

--- src/components/layout.js
+++ src/components/layout.js
@@ -63,9 +63,11 @@ class Layout extends React.Component {
         <header>{header}</header>
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          © {new Date().getFullYear()} Matthew Ardi
+          <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
+            </a>
+            <br />Except where otherwise noted, content on this site is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"> Creative Commons Attribution-ShareAlike 4.0 International License
+                </a>.
         </footer>
       </div>
     )
